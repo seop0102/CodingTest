f48ae46cc7ff7233b76b139dd9fdf70cfe924a19
seop0102 <166859473+seop0102@users.noreply.github.com>
2025-05-27 10:54:40 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+
+int solution(int angle) {
+    if ( 0 < angle && angle < 90)
+    {
+        return 1;
+    }
+    else if ( angle == 90){
+        return 2;
+    }
+    else if ( angle > 90 && angle < 180 ){
+        return 3;
+    }
+    else {
+        return 4;
+    }
+}
