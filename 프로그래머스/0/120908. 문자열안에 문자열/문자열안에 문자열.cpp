29f92b3c714468cef0bc088a9e8476a7e5aa056f
seop0102 <166859473+seop0102@users.noreply.github.com>
2025-05-28 13:29:08 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -1,11 +1,9 @@
 #include <string>
+#include <vector>
+
 using namespace std;
 
 int solution(string str1, string str2) {
-    if (str1.find(str2) != string::npos) {
-        return 1;
-    } 
-    else {
-        return 2;
-    }
+    int answer = 0;
+    return answer;
 }
