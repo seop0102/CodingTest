4bf6d3c76d870fb9113233ac5ebc31218e77e486
seop0102 <166859473+seop0102@users.noreply.github.com>
2025-05-29 09:52:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    long long a = sqrt(n);
+    if ( a * a == n ){
+        answer = (a+1) * (a+1);
+        
+    }
+    else {
+        answer=-1;
+    }
+    return answer;
+}
