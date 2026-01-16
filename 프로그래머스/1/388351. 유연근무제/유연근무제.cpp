c97df351175503335ff3a05fb5a0ac4c5026b880
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-01-17 04:02:06 +0900

    [level 1] Title: 유연근무제, Time: 0.09 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int solution(vector<int> schedules, vector<vector<int>> timelogs, int startday) {
     int answer = 0;
+    int a = (13 - startday) % 7;
+    int b = (14 - startday) % 7;
     for (int i = 0 ; i < schedules.size() ; i++) {
         schedules[i] += 10;
         if (schedules[i] % 100 >= 60 ) {
             schedules[i] += 40;
         }
-        int a = (13 - startday) % 7;
-        int b = (14 - startday) % 7;
         int total = 0;
         for (int j = 0 ; j < 7 ; j++) {
             if ( j == a || j == b) {
