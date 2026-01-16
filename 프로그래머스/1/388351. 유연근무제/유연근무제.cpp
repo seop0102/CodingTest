e53acccaa8b4865c61b6159d121535a072f76b99
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-01-17 03:36:39 +0900

    [level 1] Title: 유연근무제, Time: 0.11 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
@@ -5,20 +5,13 @@ using namespace std;
 
 int solution(vector<int> schedules, vector<vector<int>> timelogs, int startday) {
     int answer = 0;
-    // 1. 스케쥴 사이즈 = n
-    int n = schedules.size(); 
-    // 2. 스케쥴 벡터의 모든 원소에 +10, 만약 스케쥴[i]%100 >= 60 이면 -60하고 +100 (분빼고 시간)
-    for (int x = 0 ; x < n ; x++) {
-        schedules[x] += 10;
-        if (schedules[x] % 100 >= 60 ) {
-            schedules[x] -= 60; // 분
-            schedules[x] += 100; // 시간
+    for (int i = 0 ; i < schedules.size() ; i++) {
+        schedules[i] += 10;
+        if (schedules[i] % 100 >= 60 ) {
+            schedules[i] += 40;
         }
-    }
-    // 3. for 문을 0 부터 n까지 돌리기
-    for (int i = 0 ; i < n ; i++) {
-        int a = (13 - startday) % 7; //토요일
-        int b = (14 - startday) % 7; //일요일
+        int a = (13 - startday) % 7;
+        int b = (14 - startday) % 7;
         int total = 0;
         for (int j = 0 ; j < 7 ; j++) {
             if ( j == a || j == b) {
@@ -35,10 +28,5 @@ int solution(vector<int> schedules, vector<vector<int>> timelogs, int startday) 
             answer++;
         }
     }
-    // 4. 토요일 a , 일요일 b 를 정의해서 인덱스가 a나 b면 뛰어넘기
-    //    구하는 방법 : (13-startday)%7 = 토요일, (14-startday)%7 = 일요일
-    // 5. schedules[i] >= timelogs[i][j] 면 넘어가고 하나라도 아니면 바로 break.
-    // 6. 전체가 다 넘어가지면 answer ++;
-    
     return answer;
 }
