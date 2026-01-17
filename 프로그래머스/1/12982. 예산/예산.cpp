75de1c4dcd2629f39f29e0bfce281c18a54a493d
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-01-18 01:56:19 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    int total = 0;
+    for (int i = 0 ; i < d.size() ; i++) {
+        if ( total + d[i] <= budget ) {
+            answer++;
+            total += d[i];
+        } else {
+            break;
+        }
+    }
+    return answer;
+}
