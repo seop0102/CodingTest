ba32e0fd5515b42c1e2875189af36490386818c7
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-01-28 01:16:01 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 164 ms, Memory: 8688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // ----------------------------------------
+    int N;
+    cin >> N;
+    vector<vector<int>> arr;
+    for (int t = 0; t < N; t++)
+    {
+        int x, y;
+        cin >> x >> y;
+        arr.push_back({x,y});
+    }
+    sort(arr.begin(), arr.end(), [] (vector<int> a, vector<int> b)
+    {
+        
+        if (a[0] == b[0])
+        {
+            return a[1] < b[1];
+        } 
+        return a[0] < b[0];
+    });
+    for (int i = 0 ; i < N ; i++)
+    {
+        cout << arr[i][0] <<" " <<arr[i][1] << "\n";
+    }
+    // ----------------------------------------
+    return 0;
+}
