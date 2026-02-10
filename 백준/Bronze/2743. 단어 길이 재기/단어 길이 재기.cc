828f4fa646e836720956f2829f7f5adcfb9e6c37
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-02-10 11:18:54 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // ----------------------------------------
+    string A;
+    cin >> A;
+    cout << A.length();
+    // ----------------------------------------
+    return 0;
+}
