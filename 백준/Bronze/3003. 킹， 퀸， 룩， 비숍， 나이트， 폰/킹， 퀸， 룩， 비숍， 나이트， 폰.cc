7ab0cb05e7ca8376ed311cfdb8848df6f6cea649
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-02-16 01:12:27 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // ----------------------------------------
+    int K, Q, R, B, N, P;
+    cin >> K >> Q >> R >> B >> N >> P;
+    cout << 1-K << " " << 1-Q << " " << 2-R << " " << 2-B << " " << 2-N << " " << 8-P;
+    // ----------------------------------------
+    return 0;
+}
