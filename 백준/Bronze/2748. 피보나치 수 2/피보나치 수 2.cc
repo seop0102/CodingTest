38216ad950315affce4769f7ef2de3a89c00707e
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-02-20 01:48:30 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // ----------------------------------------
+    int n;
+    cin >> n;
+    vector<long long> fibo(n+1);
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    cout << fibo[n];
+    // ----------------------------------------
+    return 0;
+}
