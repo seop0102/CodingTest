59da7ebd22ffd0757474c33c69062d27e506fea0
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-02-20 01:06:11 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // ----------------------------------------
+    int N,K;
+    cin>>N>>K;
+    vector<int> arr;
+    for (int i = 1 ; i <= N; i++)
+    {
+        if ( N % i == 0)
+        {
+            arr.push_back(i);
+        }
+    }
+    if ( arr.size() < K)
+    {
+        cout << 0;
+    } else
+    {
+        cout << arr[K-1];
+    }
+    // ----------------------------------------
+    return 0;
+}
