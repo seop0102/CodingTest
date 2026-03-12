a4a3ec02f0fb2b193fe918f63593ff93d597f47c
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-03-12 17:17:49 +0900

    [Gold IV] Title: 다항 함수의 적분, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17214. 다항 함수의 적분/다항 함수의 적분.cc
+++ 백준/Gold/17214. 다항 함수의 적분/다항 함수의 적분.cc
@@ -7,51 +7,30 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    // ----------------------------------------
     string s;
     cin >> s;
-    
-    int a = 0; // x계수
-    int b = 0; // 상수
+    int a = 0; 
+    int b = 0; 
     int index = s.find('x');
-    
-    if ( s.find('x') != string::npos && isdigit(s[s.length()-1])) // ax+b
+    if ( s.find('x') != string::npos && isdigit(s[s.length()-1]))
     { 
-        if ( s[0] == 'x') a = 1; // x+b
-        else if ( s[0] == '-' && index == 1) a = -1; // -x+b
+        if ( s[0] == 'x') a = 1; 
+        else if ( s[0] == '-' && index == 1) a = -1;
         else a = stoi(s.substr(0, index));
         b = stoi (s.substr(index + 1));
-    } 
-    else // !ax+b
-    {
-        if (s[s.length()-1] == 'x') // ax
+    } else {
+        if (s[s.length()-1] == 'x')
         {
-            if (s[0] == 'x') a = 1; // x
-            else if (s[0] == '-' && index == 1 ) a = -1; // -x
-            else a = stoi(s.substr(0, s.length()-1)); // 10x
+            if (s[0] == 'x') a = 1;
+            else if (s[0] == '-' && index == 1 ) a = -1; 
+            else a = stoi(s.substr(0, s.length()-1)); 
             b = 0;
-        } 
-        else // b
-        {
+        } else {
             a = 0;
-            b = stoi(s); // 1 -1 0 10
+            b = stoi(s); 
         }
     }
-    //cout << a << b << endl;
     string answer = "";
-    /*
-    -- tc 7 --
-    2x+1
-    xx+x+W (xx+1x+W)
-
-    -- tc 8 --
-    -2x-1
-    -xx-x+W (-1xx-1x+W)
-
-    -- tc 9 --
-    -1
-    -x+W (-1x+W)
-    */
     if (a != 0 && b != 0) // ax + b
     {
         if (a/2 == 1) answer = "xx";
@@ -68,14 +47,14 @@ int main()
         }
         answer += "x+W";
     }
-    else if (a != 0 && b == 0) // ax
+    else if (a != 0 && b == 0)
     {
         if ( a/2 == 1) answer = "";
         else if ( a/2 == -1) answer = "-";
         else answer = to_string(a/2);
         answer += "xx+W";
     }
-    else if (a == 0 && b != 0) // b
+    else if (a == 0 && b != 0)
     {
         if ( b == 1) answer = "";
         else if (b == -1 ) answer = "-";
@@ -84,6 +63,5 @@ int main()
     }
     else answer = "W";
     cout << answer;
-    // ----------------------------------------
     return 0;
 }
