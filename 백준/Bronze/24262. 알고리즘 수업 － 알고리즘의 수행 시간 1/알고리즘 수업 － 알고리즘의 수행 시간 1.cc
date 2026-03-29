2804bd90eeb90dd58d9f9c3e82dd97d6f1be6b54
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-03-29 12:15:20 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // 시간 복잡도를 구하는 문제
+    int n;
+    cin >> n;
+    cout << 1 << "\n" << 0;
+    return 0;
+}
