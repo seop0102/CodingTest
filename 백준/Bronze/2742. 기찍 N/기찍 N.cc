e3e455def74cccb5dfa5b1120f6f70d752638885
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-04-17 00:31:48 +0900

    [Bronze V] Title: 기찍 N, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N;
+    cin >> N;
+    while (N > 0)
+    {
+        cout << N << "\n";
+        N--;
+    }
+    return 0;
+}
