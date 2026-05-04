e1715203e8a7de4d766a897ee377cdccf653d0e7
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-05-04 21:20:33 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = "";
+    for (int i = 0 ; i < s; i ++) {
+        answer += my_string[i];
+    }
+    answer += overwrite_string;
+    for (int i = s+overwrite_string.length(); i < my_string.length() ; i++) {
+        answer += my_string[i];
+    }
+    return answer;
+}
