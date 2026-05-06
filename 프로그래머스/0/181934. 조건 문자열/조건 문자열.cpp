b84566d705187106be945ba1c2d77c1e3c50648b
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-05-06 16:46:46 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -7,14 +7,11 @@ int solution(string ineq, string eq, int n, int m) {
     int answer = 0;
     int k = n - m;
     if ( k == 0) {
-        if ( eq == "=") answer = 1;
-        else answer = 0;
+        answer = (eq == "=");
     } else if ( k < 0 ) {
-        if (ineq == "<") answer = 1;
-        else answer = 0;
+        answer = (ineq == "<");
     } else {
-        if (ineq == ">") answer = 1;
-        else answer = 0;
+        answer = (ineq == ">");
     }
     return answer;
 }
