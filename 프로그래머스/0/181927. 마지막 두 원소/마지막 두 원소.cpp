f0f29ddfd7510cd5797fc078472264cb9990aa1f
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-05-09 00:53:51 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> num_list) {
+vector<int> solution(vector<int> v) {
     int a;
-    a = num_list[num_list.size()-1] - num_list[num_list.size()-2] > 0 ? num_list[num_list.size()-1] - num_list[num_list.size()-2] : num_list[num_list.size()-1]*2;   
-    num_list.push_back(a);
-    return num_list;
+    a = v[v.size()-1] - v[v.size()-2] > 0 ? v[v.size()-1] - v[v.size()-2] : v[v.size()-1]*2;   
+    v.push_back(a);
+    return v;
 }
