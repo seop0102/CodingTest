31d30e96685ecaee054418c389945740b707aa33
seop0102 <166859473+seop0102@users.noreply.github.com>
2026-06-15 20:05:58 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -2,18 +2,11 @@ using namespace std;
 
 long long solution(int price, int money, int count)
 {
-    //원래는 price, 그치만 n 번 타면 n*price 값, 놀이기구 타는 횟수 count
-    //ex) 20원 갖고있는데 3원짜리를 4번 탐.
-    //20 - for int i = 1; i<=4; ++i    sum += i*price 그러면
-    //3,6,9,12 = 30원이 있어야 함. 현재 재화 20이니까 10 리턴
-    long long sum = 0;
-    for (int i = 1 ; i <= count ; ++i) {
-        sum += i * price;
+    long long answer = 0;
+    for (int i = 1 ; i <= count ; i++) {
+        answer += price * i;
     }
-    if ( money - sum <0) {
-        return sum-money;
-    }
-    else {
-        return 0;
-    }
+    if ( answer-money < 0 ) answer = 0;
+    else answer -= money;
+    return answer;
 }
